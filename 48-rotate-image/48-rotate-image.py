601338c043b14c9ddef00521b1afb49b7892604f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-09 01:45:43 +0530

    Time: 38 ms (90.11%), Space: 13.8 MB (74.74%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,25 @@
+# Time: O(n^2)
+# Space: O(n^2)
+# class Solution:
+#     def rotate(self, mat: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         rotated = [[0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+#         m = len(mat[0])
+#         for i in range(len(mat)):
+#             for j in range(len(mat[0])):
+#                 rotated[j][m-i-1] = mat[i][j]
+#         mat = [row[:] for row in rotated]
+
+
+class Solution:
+    def rotate(self, mat: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        mat.reverse()
+        print(mat)
+        for i in range(len(mat)):
+            for j in range(i+1, len(mat[i])):
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
