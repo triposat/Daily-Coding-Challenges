27aa543500f87282cbda78e949a4bd76b124fd9b
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-19 20:59:30 +0530

    Time: 41 ms (71.79%), Space: 14 MB (32.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -9,18 +9,28 @@
 #                 return countPaths(i+1, j, m, n)+countPaths(i, j+1, m, n)
 #         return countPaths(0, 0, m, n)
 
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         def countPaths(i, j, m, n, dp):
+#             if i == m-1 and j == n-1:
+#                 return 1
+#             if i >= m or j >= n:
+#                 return 0
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+#             else:
+#                 dp[i][j] = countPaths(i+1, j, m, n, dp) + \
+#                     countPaths(i, j+1, m, n, dp)
+#                 return dp[i][j]
+#         dp = [[-1 for _ in range(n)] for _ in range(m)]
+#         return countPaths(0, 0, m, n, dp)
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def countPaths(i, j, m, n, dp):
-            if i == m-1 and j == n-1:
-                return 1
-            if i >= m or j >= n:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            else:
-                dp[i][j] = countPaths(i+1, j, m, n, dp) + \
-                    countPaths(i, j+1, m, n, dp)
-                return dp[i][j]
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return countPaths(0, 0, m, n, dp)
+        N = m+n-2
+        r = m-1
+        res = 1
+        for i in range(1, r+1):
+            res = res*(N-r+i)//i
+        return res
+    
