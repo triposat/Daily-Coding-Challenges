0fbe247d147250e72911375ecd8b409349b21f99
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-19 19:05:36 +0530

    Time: 42 ms (69.04%), Space: 14 MB (32.87%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,26 @@
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         def countPaths(i, j, m, n):
+#             if i == m-1 and j == n-1:
+#                 return 1
+#             if i >= m or j >= n:
+#                 return 0
+#             else:
+#                 return countPaths(i+1, j, m, n)+countPaths(i, j+1, m, n)
+#         return countPaths(0, 0, m, n)
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def countPaths(i, j, m, n, dp):
+            if i == m-1 and j == n-1:
+                return 1
+            if i >= m or j >= n:
+                return 0
+            if dp[i][j] != -1:
+                return dp[i][j]
+            else:
+                dp[i][j] = countPaths(i+1, j, m, n, dp) + \
+                    countPaths(i, j+1, m, n, dp)
+                return dp[i][j]
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        return countPaths(0, 0, m, n, dp)
