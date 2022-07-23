677ba04c9792f6183e715ca88332d122c1bd00b2
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-23 22:59:08 +0530

    Time: 1611 ms (60.00%), Space: 24.6 MB (40.00%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        cnt=0
+        ans=0
+        for ele in nums:
+            if ele!=0:
+                ans+=(cnt*(cnt+1)//2)
+                cnt=0
+            else:
+                cnt+=1
+        ans+=(cnt*(cnt+1)//2)
+        return ans
