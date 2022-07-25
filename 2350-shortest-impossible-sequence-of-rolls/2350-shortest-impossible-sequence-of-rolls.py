3844b396a8917b7f15f7b239f2bee2094d70f1d6
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-26 00:42:26 +0530

    Time: 1567 ms (33.33%), Space: 28.3 MB (33.33%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
@@ -1,13 +1,10 @@
 class Solution:
     def shortestSequence(self, rolls: List[int], k: int) -> int:
         s = set()
-        hey = 0
-
-        for r in rolls:
-            s.add(r)
-            if len(s) == k:        # All possible number has appeared once.
-                # So you must "at least" use one more place to store it.
-                hey += 1
-                s.clear()          # Clear the set.
-
-        return hey + 1
+        ans = 0
+        for rol in rolls:
+            s.add(rol)
+            if len(s) == k:
+                ans += 1
+                s.clear()
+        return ans+1
