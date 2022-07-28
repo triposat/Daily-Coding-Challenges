f36de81a2bd587dd43e89af55700a2fc06a67782
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-28 12:12:30 +0530

    Time: 483 ms (38.77%), Space: 16.6 MB (73.49%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        add = 0
+        hmap = {0: 1}
+        ans = 0
+        for ele in nums:
+            add += ele
+            if add-k in hmap:
+                ans += hmap[add-k]
+            hmap[add] = hmap.get(add, 0)+1
+        return ans
