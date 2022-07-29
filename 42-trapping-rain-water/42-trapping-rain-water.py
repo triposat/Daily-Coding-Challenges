12134193021743206c9a32c537ab4518192417a3
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-29 15:37:05 +0530

    Time: 204 ms (44.91%), Space: 16 MB (41.59%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, arr: List[int]) -> int:
+        n = len(arr)
+        left = 0
+        right = n-1
+        leftbar = arr[0]
+        rightbar = arr[n-1]
+        ans = 0
+        while left < right:
+            if leftbar < rightbar:
+                if arr[left] > leftbar:
+                    leftbar = arr[left]
+                else:
+                    ans += leftbar-arr[left]
+                    left += 1
+            else:
+                if arr[right] > rightbar:
+                    rightbar = arr[right]
+                else:
+                    ans += rightbar-arr[right]
+                    right -= 1
+        return ans
