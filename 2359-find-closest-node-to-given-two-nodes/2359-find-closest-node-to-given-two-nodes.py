ce86576d4f0620fbefe00c435e71ee5b582742b8
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-01 15:24:03 +0530

    Time: 2139 ms (40.00%), Space: 33.4 MB (20.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,24 +1,24 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         def bfs(start):
-            n=len(edges)
-            dist=[-1]*n
-            q=[(start, 0)]
-            seen=set([start])
+            n = len(edges)
+            dist = [-1]*n
+            q = [(start, 0)]
+            seen = set([start])
             while q:
                 ele, dis = q.pop(0)
                 dist[ele] = dis
                 newDis = dis+1
-                if edges[ele]!=-1 and edges[ele] not in seen:
+                if edges[ele] != -1 and edges[ele] not in seen:
                     q.append((edges[ele], newDis))
                     seen.add(edges[ele])
             return dist
-        dist1=bfs(node1)
-        dist2=bfs(node2)
+        dist1 = bfs(node1)
+        dist2 = bfs(node2)
         minDis = 1e9
-        ans=-1
+        ans = -1
         for i in range(len(edges)):
-            if min(dist1[i], dist2[i])>=0 and max(dist1[i], dist2[i])<minDis:
+            if min(dist1[i], dist2[i]) >= 0 and max(dist1[i], dist2[i]) < minDis:
                 minDis = max(dist1[i], dist2[i])
-                ans=i
+                ans = i
         return ans
