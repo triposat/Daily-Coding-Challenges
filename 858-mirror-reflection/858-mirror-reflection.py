e17b5bf45ddc3a4083548b9cb8f65428b3985cf1
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-04 16:06:21 +0530

    Time: 63 ms (15.84%), Space: 13.8 MB (63.37%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        while p % 2 == 0 and q % 2 == 0:
+            p //= 2
+            q //= 2
+        return 1-p % 2+q % 2
