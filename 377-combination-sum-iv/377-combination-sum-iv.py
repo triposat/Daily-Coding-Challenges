5a425559d851887db32aee3bbc3a41c8b95cc98a
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-08 23:10:28 +0530

    Time: 65 ms (48.33%), Space: 14 MB (48.19%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,16 +1,16 @@
 class Solution:
-    def dfs(self, dp, nums, tar, res):
+    def dfs(self, dp, nums, tar):
         if dp[tar] != -1:
             return dp[tar]
-        res=0
+        res = 0
         for ele in nums:
             if ele <= tar:
-                res += self.dfs(dp, nums, tar-ele, res)
+                res += self.dfs(dp, nums, tar-ele)
         dp[tar] = res
         return dp[tar]
 
     def combinationSum4(self, nums: List[int], tar: int) -> int:
         dp = [-1]*(tar+1)
         dp[0] = 1
-        self.dfs(dp, nums, tar, 0)
+        self.dfs(dp, nums, tar)
         return dp[tar]
