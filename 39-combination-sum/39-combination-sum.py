355bf96e752daf4030c71e4ce5b71c4f4e4da58b
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-08 14:09:26 +0530

    Time: 163 ms (31.68%), Space: 13.9 MB (92.52%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def dfs(self, arr, tar, idx, path, res):
-        if tar<0:
-            return 
-        if tar==0:
+        if tar < 0:
+            return
+        if tar == 0:
             res.append(path)
             return
         for i in range(idx, len(arr)):
             self.dfs(arr, tar-arr[i], i, path+[arr[i]], res)
+
     def combinationSum(self, arr: List[int], tar: int) -> List[List[int]]:
-        res=[]
+        res = []
         arr.sort()
         self.dfs(arr, tar, 0, [], res)
         return res
