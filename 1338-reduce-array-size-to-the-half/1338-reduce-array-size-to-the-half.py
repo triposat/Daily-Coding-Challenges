6ae355061045a4da0d55acebb96a8578c4b5c9fc
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-18 13:49:45 +0530

    Time: 953 ms (52.22%), Space: 32.2 MB (67.22%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -3,12 +3,10 @@ class Solution:
         hmap = Counter(arr)
         heap = list(-i for i in hmap.values())
         heapify(heap)
-        # print(heap)
-        cnt=0
-        n=len(arr)//2
-        ans=0
-        while cnt<n:
-            cnt+=-1*heappop(heap)
-            # print(cnt)
-            ans+=1
+        cnt = 0
+        n = len(arr)//2
+        ans = 0
+        while cnt < n:
+            cnt += -1*heappop(heap)
+            ans += 1
         return ans
