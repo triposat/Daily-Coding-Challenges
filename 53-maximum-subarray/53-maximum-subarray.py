2f74d90318d917168ba5914bacacf8281fb2a395
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-25 18:22:35 +0530

    Time: 1257 ms (34.38%), Space: 27.9 MB (78.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,25 +1,10 @@
-# Brute Force Approach
-# Time: O(n^2), Space: O(1)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        ans = -float('inf')
-        for i in range(n):
-            curSum = 0
-            for j in range(i, n):
-                curSum += nums[j]
-                ans = max(ans, curSum)
+        add=0
+        ans=-float("inf")
+        for ele in nums:
+            add+=ele
+            ans=max(ans, add)
+            if add<0:
+                add=0
         return ans
-
-# Kadane's Algorithm
-# Time: O(n), Space: O(1)
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = -float('inf')
-        curSum = 0
-        for num in nums:
-            curSum += num
-            maxSum = max(maxSum, curSum)
-            if curSum < 0:
-                curSum = 0
-        return maxSum
