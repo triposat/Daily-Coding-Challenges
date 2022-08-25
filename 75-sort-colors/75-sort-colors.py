20b29cad702654bf94fb26063c938b2c9a58ff99
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-25 14:14:32 +0530

    Time: 39 ms (85.34%), Space: 13.8 MB (64.49%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,19 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n0, n1, n2 = -1, -1, -1
-        for i in range(len(nums)):
-            if nums[i] == 0:
+        for num in nums:
+            if num == 0:
                 n2 += 1
                 nums[n2] = 2
                 n1 += 1
                 nums[n1] = 1
                 n0 += 1
                 nums[n0] = 0
-            elif nums[i] == 1:
+            elif num == 1:
                 n2 += 1
                 nums[n2] = 2
                 n1 += 1
                 nums[n1] = 1
-            elif nums[i] == 2:
+            elif num == 2:
                 n2 += 1
                 nums[n2] = 2
