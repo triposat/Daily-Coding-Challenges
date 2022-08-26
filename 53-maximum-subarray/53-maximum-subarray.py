6aa549633178040f778d5905b8600711ab6d48ac
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-26 10:00:28 +0530

    Time: 1149 ms (47.22%), Space: 27.8 MB (78.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,11 +4,4 @@ class Solution:
         suf=nums[:]
         for i in range(1, len(nums)):
             pref[i]+=max(0, pref[i-1])
-        for i in range(len(nums)-2, -1, -1):
-            suf[i]+=max(0, suf[i+1])
-        def ans(arr, l, r):
-            if l == r:
-                return arr[l]
-            mid = (l+r)//2
-            return max(ans(arr, l, mid), ans(arr, mid+1, r), pref[mid]+suf[mid+1])
-        return ans(nums, 0, len(nums)-1)
+        return max(pref)
