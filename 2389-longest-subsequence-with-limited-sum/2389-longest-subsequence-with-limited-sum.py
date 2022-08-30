54dcf4b04797cec431698a7db57adea2bfa050e6
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-30 18:08:25 +0530

    Time: 202 ms (66.28%), Space: 14.3 MB (41.26%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         def bise(arr, q):
-            st=0
-            end=len(arr)
-            while st<end:
-                mid=(st+end)>>1
-                if arr[mid]==q:
+            st = 0
+            end = len(arr)
+            while st < end:
+                mid = (st+end) >> 1
+                if arr[mid] == q:
                     return mid+1
-                elif arr[mid]<q:
-                    st=mid+1
+                elif arr[mid] < q:
+                    st = mid+1
                 else:
-                    end=mid
+                    end = mid
             return st
         nums.sort()
         for i in range(1, len(nums)):
-            nums[i]=nums[i-1]+nums[i]
+            nums[i] = nums[i-1]+nums[i]
         return [bise(nums, q) for q in queries]
