be567f9f00fc0e148f8516d1ff6e8f8a0f6986e5
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-08-30 14:05:51 +0530

    Time: 594 ms (99.17%), Space: 27.5 MB (90.74%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,12 +1,11 @@
-# Sliding window approach
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
         mx = 0
         n = len(nums)
         i = 0
         while i < n:
-            negS = -1  # It will store the index of first negative number
-            negE = -1  # It will store the last index of negative number
+            negS = -1  # It will store the index of the first negative number
+            negE = -1  # It will store the last index of the negative number
             pos = 0
             neg = 0
             j = i
@@ -18,7 +17,7 @@ class Solution:
                     negE = j
                 if nums[j] < 0 and negS == -1:
                     negS = j
-                elif nums[j] == 0:  # End the subrray, because after that all the product will be 0
+                elif nums[j] == 0:  # End the subarray because after that all the products will be 0
                     break
                 j += 1
             if not neg & 1:  # If negative numbers are even
