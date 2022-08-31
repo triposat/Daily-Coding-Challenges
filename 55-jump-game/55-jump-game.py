4335a8054ebcca39310d96a968b9eb46a8aa038f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-01 00:49:11 +0530

    Time: 1336 ms (12.52%), Space: 15.2 MB (49.18%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,8 +1,22 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n=len(nums)
+#         last=n-1
+#         for i in range(n-2, -1, -1):
+#             if i+nums[i]>=last:
+#                 last=i
+#         return True if last==0 else False
+    
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n=len(nums)
-        last=n-1
-        for i in range(n-2, -1, -1):
-            if i+nums[i]>=last:
-                last=i
-        return True if last==0 else False
+        curr = 0
+        jumps = 0
+        far = 0
+        for i in range(len(nums)-1):
+            far = max(far, i+nums[i])
+            if i == curr:
+                curr = far
+                jumps += 1
+            if i >= far:
+                return False
+        return True
