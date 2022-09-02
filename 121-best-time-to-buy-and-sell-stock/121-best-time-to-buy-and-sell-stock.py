a59cfc27ade296159ff862c5b32242ce9fa35566
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-02 22:33:14 +0530

    Time: 1668 ms (45.25%), Space: 25 MB (38.19%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,10 @@
-# Time: O(n^2)
-# Space: O(1)
-# Time Limit Exceeded 
-
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         maxSoFar = 0
-#         for i in range(len(prices)):
-#             for j in range(i+1, len(prices)):
-#                 if prices[j] > prices[i]:
-#                     maxSoFar = max(maxSoFar, prices[j]-prices[i])
-#         return maxSoFar
-    
-    
-# Kadane's Algo
-# Time: O(n), Space: O(1)
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxCur = 0
-        maxSo = 0
-        for i in range(1, len(prices)):
-            maxCur += prices[i]-prices[i-1]
-            if maxCur < 0:
-                maxCur = 0
-            maxSo = max(maxSo, maxCur)
-        return maxSo
+    def maxProfit(self, p: List[int]) -> int:
+        ans = 0
+        res = 0
+        for i in range(1, len(p)):
+            ans += p[i]-p[i-1]
+            if ans < 0:
+                ans = 0
+            res = max(res, ans)
+        return res
