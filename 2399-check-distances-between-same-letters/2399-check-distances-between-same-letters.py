3f43ab9ca7e7557b61a266f8a5315061a39268d0
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-06 01:49:03 +0530

    Time: 96 ms (12.27%), Space: 14 MB (21.68%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
@@ -1,9 +1,9 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
-        freq=defaultdict(list)
+        freq = defaultdict(list)
         for i in range(len(s)):
             freq[s[i]].append(i)
-        
+
         for char, (a, b) in freq.items():
             if not b-a-1 == distance[ord(char)-ord('a')]:
                 return False
