b70e028336c95eb81febe317c529650869df9eeb
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-10 22:10:57 +0530

    Time: 31 ms (98.71%), Space: 13.8 MB (81.47%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.py
+++ 1006-clumsy-factorial/1006-clumsy-factorial.py
@@ -12,22 +12,19 @@
 #                 stack.append(-n+i)
 #         return sum(stack)
 
-
 class Solution:
     def clumsy(self, n: int) -> int:
-        if n==1:
+        if n == 1:
             return 1
-        if n==2:
+        if n == 2:
             return 2
-        if n==3:
+        if n == 3:
             return 6
-        if n==4:
+        if n == 4:
             return 7
-        if n%4==3:
-            return n-1
-        if n%4==0:
+        if n % 4 == 0:
             return n+1
-        if n%4==1:
+        if n % 4 == 1 or n % 4 == 2:
             return n+2
-        if n%4==2:
-            return n+2
+        if n % 4 == 3:
+            return n-1
