ab20718309331dee2e4f5ab66314a6f1ad8d9aa3
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-14 01:53:37 +0530

    Time: 125 ms (78.36%), Space: 14.3 MB (13.40%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,10 +1,10 @@
-from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        for i in range(n):
-            # Here we pass three arg's, which means find the position of nums[i] in the nums array within index i.
-            idx = bisect_left(nums, nums[i], hi=i)
-            if idx != i:
-                nums[idx], nums[i] = nums[i], float(inf)
-        return nums.index(float(inf)) if float(inf) in nums else n
+        res = []
+        for ele in nums:
+            idx = bisect.bisect_left(res, ele)
+            if idx == len(res):
+                res.append(ele)
+            else:
+                res[idx] = ele
+        return len(res)
