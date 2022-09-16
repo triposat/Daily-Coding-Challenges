de8548254b3ef6f225d7ba864a6805b235529b03
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-16 22:52:42 +0530

    Time: 119 ms (54.75%), Space: 15.4 MB (51.15%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
@@ -1,12 +1,20 @@
+# class Solution:
+#     def findLengthOfLCIS(self, nums: List[int]) -> int:
+#         ans = 1
+#         cnt = 1
+#         for i in range(1, len(nums)):
+#             if nums[i] > nums[i-1]:
+#                 cnt += 1
+#             else:
+#                 ans = max(ans, cnt)
+#                 cnt = 1
+#         ans = max(ans, cnt)
+#         return ans
+
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
-        ans = 1
-        cnt = 1
+        dp=[1]*len(nums)
         for i in range(1, len(nums)):
-            if nums[i] > nums[i-1]:
-                cnt += 1
-            else:
-                ans = max(ans, cnt)
-                cnt = 1
-        ans = max(ans, cnt)
-        return ans
+            if nums[i]>nums[i-1]:
+                dp[i]=dp[i-1]+1
+        return max(dp)
