cfa055bdf7edc7193c8c83da6c9007743f020258
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-16 23:18:25 +0530

    Time: 62 ms (57.04%), Space: 13.9 MB (97.57%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, path, res):
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+            return res
+
+        res=[]
+        return dfs(nums, [], res)
+        return res
