0d0358e4b1c9753f54c94e8a2b0851a22acf7ec7
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-20 20:34:14 +0530

    Time: 1286 ms (22.08%), Space: 24.7 MB (48.19%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,11 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        small, big =(1<<31), (1<<31)
+        small, big = (1 << 31), (1 << 31)
         for ele in nums:
-            if ele<=small:
-                small=ele
-            elif ele<=big:
-                big=ele
+            if ele <= small:
+                small = ele
+            elif ele <= big:
+                big = ele
             else:
                 return True
         return False
