58c7820dfeffa7695e83201826e6a984e7c2c040
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-20 18:16:31 +0530

    Time: 28 ms (98.31%), Space: 14.1 MB (21.07%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.py
+++ 2409-count-days-spent-together/2409-count-days-spent-together.py
@@ -1,17 +1,17 @@
 class Solution:
     def countDaysTogether(self, arAl: str, lAl: str, arBo: str, lBo: str) -> int:
-        months=[-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        start=max(arAl, arBo)
-        end=min(lAl, lBo)
+        months = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        start = max(arAl, arBo)
+        end = min(lAl, lBo)
+
+        startDay = int(start[3:])
+        startMonth = int(start[:2])
+        endDay = int(end[3:])
+        endMonth = int(end[:2])
         
-        startDay=int(start[3:])
-        startMonth=int(start[:2])
-        endDay=int(end[3:])
-        endMonth=int(end[:2])
-        
-        if start>end:
+        if start > end:
             return 0
-        if startMonth==endMonth:
+        if startMonth == endMonth:
             return endDay-startDay+1
-        elif startMonth<endMonth:
+        elif startMonth < endMonth:
             return months[startMonth]-startDay + endDay + 1 + sum(months[m] for m in range(startMonth+1, endMonth))
