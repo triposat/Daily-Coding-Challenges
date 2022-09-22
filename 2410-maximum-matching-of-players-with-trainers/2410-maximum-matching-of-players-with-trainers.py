bf5f3055b45082bc09ae208292d70e9331e16ea4
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-22 12:11:25 +0530

    Time: 1777 ms (61.48%), Space: 33 MB (7.05%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -1,31 +1,36 @@
+# Using Sorting
 class Solution:
-    def matchPlayersAndTrainers(self, p: List[int], q: List[int]) -> int:
-#         p.sort()
-#         q.sort()
-        
-#         i=len(p)-1
-#         j=len(q)-1
-#         cnt=0
-#         while i>-1 and j>-1:
-#             if p[i]>q[j]:
-#                 i-=1
-#             elif p[i]<=q[j]:
-#                 cnt+=1
-#                 j-=1
-#                 i-=1
-#         return cnt
-        p=list(map(lambda x: -1*x, p))
-        q=list(map(lambda x: -1*x, q))
-        heapify(p)
-        heapify(q)
-        cnt=0
-        while p and q:
-            p1=-1*p[0]
-            q1=-1*q[0]
-            if p1<=q1:
-                cnt+=1
-                heappop(p)
-                heappop(q)
+    def matchPlayersAndTrainers(self, play, trai):
+        play.sort()
+        trai.sort()
+
+        i = len(play)-1
+        j = len(trai)-1
+        cnt = 0
+        while i > -1 and j > -1:
+            if play[i] > trai[j]:
+                i -= 1
+            elif play[i] <= trai[j]:
+                cnt += 1
+                j -= 1
+                i -= 1
+        return cnt
+
+# Using Priority Queue
+class Solution:
+    def matchPlayersAndTrainers(self, play, trai):
+        play = list(map(lambda x: -1*x, play))
+        trai = list(map(lambda x: -1*x, trai))
+        heapify(play)
+        heapify(trai)
+        cnt = 0
+        while play and trai:
+            p1 = -1*play[0]
+            q1 = -1*trai[0]
+            if p1 <= q1:
+                cnt += 1
+                heappop(play)
+                heappop(trai)
             else:
-                heappop(p)
+                heappop(play)
         return cnt
