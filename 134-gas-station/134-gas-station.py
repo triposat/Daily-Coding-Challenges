e3e28e98822c61ebdcd2f727fd87d40e98521f58
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-09-30 18:53:52 +0530

    Time: 663 ms (98.57%), Space: 19.1 MB (74.15%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,19 +1,19 @@
-# class Solution:
-#     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-#         n = len(gas)
-#         for i in range(len(gas)):
-#             j = i
-#             fuel = 0
-#             cnt = 0
-#             while cnt < len(cost):
-#                 fuel += gas[j % n]-cost[j % n]
-#                 if fuel < 0:
-#                     break
-#                 j += 1
-#                 cnt += 1
-#             if cnt == n and fuel >= 0:
-#                 return i
-#         return -1
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        for i in range(len(gas)):
+            j = i
+            fuel = 0
+            cnt = 0
+            while cnt < len(cost):
+                fuel += gas[j % n]-cost[j % n]
+                if fuel < 0:
+                    break
+                j += 1
+                cnt += 1
+            if cnt == n and fuel >= 0:
+                return i
+        return -1
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
