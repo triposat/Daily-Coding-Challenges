ffaa752dc59471a385264f71233b0269a00fef80
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-10-04 02:11:50 +0530

    Time: 6432 ms (42.55%), Space: 166.1 MB (62.60%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -1,13 +1,31 @@
+class Node:
+    def __init__(self):
+        self.child = {}
+        self.cnt = 0
+        
+class Trie:
+    def __init__(self):
+        self.trie = Node()
+
+    def insert(self, word):
+        node = self.trie
+        for ch in word:
+            if ch not in node.child:
+                node.child[ch] = Node()
+            node = node.child[ch]
+            node.cnt += 1
+
+    def count(self, word):
+        ans = 0
+        node = self.trie
+        for ch in word:
+            ans += node.child[ch].cnt
+            node = node.child[ch]
+        return ans
+
 class Solution:
-    def sumPrefixScores(self, words: List[str]) -> List[int]:
-        hmap = defaultdict(int)
-        for w in words:
-            for i in range(len(w)):
-                hmap[w[:i+1]] += 1
-        res = []
-        for w in words:
-            ans = 0
-            for i in range(len(w)):
-                ans += hmap[w[:i+1]]
-            res.append(ans)
-        return res
+    def sumPrefixScores(self, words):
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        return [trie.count(word) for word in words]
