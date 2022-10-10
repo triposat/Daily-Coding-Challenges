b3c6940d377228ff1f73c8b69e10884fc8b39ba1
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-10-11 01:38:49 +0530

    Time: 316 ms (19.80%), Space: 14.5 MB (11.10%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        ans=[]
+        ans = []
         for n, h in zip(names, heights):
             ans.append((-1*h, n))
         heapify(ans)
-        res=[]
+        res = []
         while ans:
             res.append(heappop(ans)[1])
         return res
